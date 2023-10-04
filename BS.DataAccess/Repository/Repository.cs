00b69ac3e0d2573a4ce22bad0d1a1baa51e26970
Zeyad01 <zeyad.elmahdy@gmail.com>
2023-10-04 03:58:44 +0300

    Navigation Properties

--- BS.DataAccess/Repository/Repository.cs
+++ BS.DataAccess/Repository/Repository.cs
@@ -20,28 +20,61 @@ namespace BS.DataAccess.Repository
         {
             _db = db;
             dbSet = _db.Set<T>();
-            //_db.Categories==dbSet
+			//_db.Categories==dbSet
 
-        }
-        public void Add(T entity)
+			_db.Products.Include(u => u.Category).Include(u => u.CategoryId);
+
+		}
+		public void Add(T entity)
         {
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
-        {
-            IQueryable<T> query = dbSet;
-            query = query.Where(filter);
-            return query.FirstOrDefault();
-        }
+		public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+		{
+			IQueryable<T> query;
+			if (tracked)
+			{
+				query = dbSet;
 
-        public IEnumerable<T> GetAll()
-        {
-            IQueryable<T> query = dbSet;
-            return query.ToList();
-        }
+			}
+			else
+			{
+				query = dbSet.AsNoTracking();
+			}
 
-        public void Remove(T entity)
+			query = query.Where(filter);
+			if (!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach (var includeProp in includeProperties
+					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
+			return query.FirstOrDefault();
+
+		}
+
+		public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
+		{
+			IQueryable<T> query = dbSet;
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+			if (!string.IsNullOrEmpty(includeProperties))
+			{
+				foreach (var includeProp in includeProperties
+					.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
+			return query.ToList();
+		}
+
+		public void Remove(T entity)
         {
             dbSet.Remove(entity);
         }
