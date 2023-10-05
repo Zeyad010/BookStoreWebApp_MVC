9b2e95671c992978275bb4b119038462fa4fa547
Zeyad01 <zeyad.elmahdy@gmail.com>
2023-10-05 11:18:11 +0300

    Add ShoppingCart Model & Repo

---
+++ BS.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,27 @@
+﻿using BS.DataAccess.Repository.IRepository;
+using BS.DataAccess.Data;
+using BS.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
