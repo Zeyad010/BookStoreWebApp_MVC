d0362f3fcc6ef6f1ec4dd2435e2fefa99ea324b1
Zeyad01 <zeyad.elmahdy@gmail.com>
2023-10-07 01:41:43 +0300

    DbInitializer

--- BookStoreWebApp/Program.cs
+++ BookStoreWebApp/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using BS.Utility;
 using Stripe;
 using Microsoft.Data.SqlClient;
+using BS.DataAccess.DbInitializr;
+using BS.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,8 +48,7 @@ builder.Services.AddSession(options => {
     options.Cookie.IsEssential = true;
 });
 
-
-
+builder.Services.AddScoped<IDbInitializer,DbInitializer>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
@@ -77,6 +78,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseSession();
+SeedDatabase();
 
 app.MapRazorPages();
 
@@ -87,3 +89,14 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+         var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+
+    }
+}
